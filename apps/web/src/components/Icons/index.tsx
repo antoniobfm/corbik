e4a91b03531a1a647b91e7d66e8b5103380dba0e
antoniobfm <toinho111@gmail.com>
2022-10-16 13:10:48 -0300

    Created Icons

--- apps/web/src/components/Icons/index.tsx
+++ apps/web/src/components/Icons/index.tsx
@@ -1,18 +1,14 @@
 export const KeyOutlined = () => (
   <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g opacity="0.5">
-  <path d="M15.6671 11.1977C14.0362 12.7352 11.7008 13.2077 9.6504 12.6002L5.92165 16.1252C5.65248 16.3877 5.12207 16.5452 4.74207 16.4927L3.01623 16.2677C2.44623 16.1927 1.91582 15.6827 1.82873 15.1427L1.59123 13.5077C1.53582 13.1477 1.7179 12.6452 1.97915 12.3902L5.69998 8.86523C5.06665 6.91523 5.55748 4.70273 7.18832 3.16523C9.52373 0.952735 13.3158 0.952735 15.6592 3.16523C18.0025 5.37773 18.0025 8.98523 15.6671 11.1977Z" stroke="#E4E3E8" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M5.45456 13.1172L7.27539 14.8422" stroke="#E4E3E8" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M11.4792 8.25C12.135 8.25 12.6667 7.74632 12.6667 7.125C12.6667 6.50368 12.135 6 11.4792 6C10.8233 6 10.2917 6.50368 10.2917 7.125C10.2917 7.74632 10.8233 8.25 11.4792 8.25Z" stroke="#E4E3E8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-  </g>
+  <path d="M15.6671 11.1977C14.0362 12.7352 11.7008 13.2077 9.6504 12.6002L5.92165 16.1252C5.65248 16.3877 5.12207 16.5452 4.74207 16.4927L3.01623 16.2677C2.44623 16.1927 1.91582 15.6827 1.82873 15.1427L1.59123 13.5077C1.53582 13.1477 1.7179 12.6452 1.97915 12.3902L5.69998 8.86523C5.06665 6.91523 5.55748 4.70273 7.18832 3.16523C9.52373 0.952735 13.3158 0.952735 15.6592 3.16523C18.0025 5.37773 18.0025 8.98523 15.6671 11.1977Z" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M5.45456 13.1172L7.27539 14.8422" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M11.4792 8.25C12.135 8.25 12.6667 7.74632 12.6667 7.125C12.6667 6.50368 12.135 6 11.4792 6C10.8233 6 10.2917 6.50368 10.2917 7.125C10.2917 7.74632 10.8233 8.25 11.4792 8.25Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 )
 
 export const SmsOutlined = () => (
   <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g opacity="0.5">
-  <path d="M12.75 15.375H5.25C3 15.375 1.5 14.25 1.5 11.625V6.375C1.5 3.75 3 2.625 5.25 2.625H12.75C15 2.625 16.5 3.75 16.5 6.375V11.625C16.5 14.25 15 15.375 12.75 15.375Z" stroke="#E4E3E8" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M12.75 6.75L10.4025 8.625C9.63 9.24 8.3625 9.24 7.59 8.625L5.25 6.75" stroke="#E4E3E8" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-  </g>
+  <path d="M12.75 15.375H5.25C3 15.375 1.5 14.25 1.5 11.625V6.375C1.5 3.75 3 2.625 5.25 2.625H12.75C15 2.625 16.5 3.75 16.5 6.375V11.625C16.5 14.25 15 15.375 12.75 15.375Z" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M12.75 6.75L10.4025 8.625C9.63 9.24 8.3625 9.24 7.59 8.625L5.25 6.75" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 )
