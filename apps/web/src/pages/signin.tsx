657804d85907dabb2781d1613e9167f433edb7f0
antoniobfm <toinho111@gmail.com>
2022-10-16 13:13:09 -0300

    Wrapped in Everything Component

--- apps/web/src/pages/signin.tsx
+++ apps/web/src/pages/signin.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 import { Container } from '@/modules/SignIn/styles';
 import { Header } from '@/modules/SignIn/Header';
 import { Form } from '@/modules/SignIn/Form';
+import { Everything } from '@/components/Container/styles';
+import { Footer } from '@/modules/SignIn/Footer';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Header />
-      <Form />
-    </Container>
+    <Everything>
+      <Container>
+        <Header />
+        <Form />
+        <Footer />
+      </Container>
+    </Everything>
   )
 }
 
