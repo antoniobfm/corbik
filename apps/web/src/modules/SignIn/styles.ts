faede1df01223d700bc72aafe8bc56684e7c66eb
antoniobfm <toinho111@gmail.com>
2022-10-16 13:14:00 -0300

    Implemented grid to space between modules

--- apps/web/src/modules/SignIn/styles.ts
+++ apps/web/src/modules/SignIn/styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: grid;
+  row-gap: 32px;
+
+  padding: 24px;
+
   h1 {
     text-shadow: 0px 0px 77px rgba(228, 227, 232, 0.4);
   }
