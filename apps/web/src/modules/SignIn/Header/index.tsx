36c3ff0a1bec2a0641e0fc61a8d45a84b91dc523
antoniobfm <toinho111@gmail.com>
2022-10-23 22:58:00 -0300

    Updated MiniButton to be a link insted of using next/router

--- apps/web/src/modules/SignIn/Header/index.tsx
+++ apps/web/src/modules/SignIn/Header/index.tsx
@@ -1,16 +1,24 @@
 import { MiniButton } from '@/components/MiniButton';
 import { UserSquare } from 'iconsax-react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { Container } from './styles';
 
 export const Header: React.FC = () => {
+  const router = useRouter();
+
   return (
     <Container>
-      <MiniButton 
-        text="Sign Up" 
-        icon={<UserSquare size="16" color="white" variant="Bulk" />} 
-      />
+      <Link href="/signup">
+        <a>
+          <MiniButton
+            text="Create Account"
+            icon={<UserSquare size="16" color="white" variant="Bulk" />}
+          />
+        </a>
+      </Link>
       <h1>Sign in to your account</h1>
     </Container>
   )
