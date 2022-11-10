ea9228e788f3919f76448d219be4f9820fa83fde
antoniobfm <toinho111@gmail.com>
2022-11-10 17:21:18 -0300

    Added router push to forgot your password

--- apps/web/src/modules/SignIn/Footer/index.tsx
+++ apps/web/src/modules/SignIn/Footer/index.tsx
@@ -1,13 +1,16 @@
 import { TertiaryButton } from '@/components/TertiaryButton';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { Container } from './styles';
 
 export const Footer: React.FC = () => {
+  const router = useRouter();
+
   return (
     <Container>
       <div className="divider" />
-      <TertiaryButton text="Forgot your password?" />
+      <TertiaryButton onClick={() => router.push('/forgot-password')} text="Forgot your password?" />
     </Container>
   )
 }
