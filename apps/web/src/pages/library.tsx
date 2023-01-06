fa1b13964828f82e40f20fe8383ca1888ea09bcf
antoniobfm <toinho111@gmail.com>
2023-01-06 09:35:04 -0300

    Created Library page

---
+++ apps/web/src/pages/library.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from '@/modules/Library/styles';
+
+const Library: React.FC = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Library;
