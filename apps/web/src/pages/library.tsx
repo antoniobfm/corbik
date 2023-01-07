e3f77d7f7815deddef2b650ce1269ebcaa25542e
antoniobfm <toinho111@gmail.com>
2023-01-07 18:18:47 -0300

    Added Public Library module to Food Library page

--- apps/web/src/pages/library.tsx
+++ apps/web/src/pages/library.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Container } from '@/modules/Library/styles';
 import Header from '@/modules/Library/Header';
 import MyLibrary from '@/modules/Library/MyLibrary';
+import PublicLibrary from '@/modules/Library/PublicLibrary';
 
 const Library: React.FC = () => {
   return (
     <Container>
       <Header />
       <MyLibrary />
+      <PublicLibrary />
     </Container>
   )
 }
