f0bf6a2c4994fe6dd6d2a24ee04f3600c5fe157e
antoniobfm <toinho111@gmail.com>
2023-01-07 18:01:38 -0300

    Added MyLibrary to Food Library

--- apps/web/src/pages/library.tsx
+++ apps/web/src/pages/library.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Container } from '@/modules/Library/styles';
 import Header from '@/modules/Library/Header';
+import MyLibrary from '@/modules/Library/MyLibrary';
 
 const Library: React.FC = () => {
   return (
     <Container>
       <Header />
+      <MyLibrary />
     </Container>
   )
 }
