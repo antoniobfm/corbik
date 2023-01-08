ae7f3bf4a5794890d8e539006fb3ec729ea22271
antoniobfm <toinho111@gmail.com>
2023-01-07 21:21:20 -0300

    Added SearchBar module component to Food Library

--- apps/web/src/pages/library.tsx
+++ apps/web/src/pages/library.tsx
@@ -4,11 +4,13 @@ import { Container } from '@/modules/Library/styles';
 import Header from '@/modules/Library/Header';
 import MyLibrary from '@/modules/Library/MyLibrary';
 import PublicLibrary from '@/modules/Library/PublicLibrary';
+import SearchBar from '@/modules/Library/SearchBar';
 
 const Library: React.FC = () => {
   return (
     <Container>
       <Header />
+      <SearchBar />
       <MyLibrary />
       <PublicLibrary />
     </Container>
