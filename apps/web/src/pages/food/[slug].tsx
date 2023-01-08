bed97240529da75eb83dc5357b1cb5a7a2aa1898
antoniobfm <toinho111@gmail.com>
2023-01-08 19:12:20 -0300

    Created Food Page

---
+++ apps/web/src/pages/food/[slug].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from '@/modules/Food/styles';
+
+const food: React.FC = () => {
+  return (
+    <Container>
+    
+    </Container>
+  )
+}
+
+export default food;
