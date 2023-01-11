739ec95d27ca7faaa80eddb7770bf97cba3f012f
antoniobfm <toinho111@gmail.com>
2023-01-11 18:06:31 -0300

    Created Ingredient component

---
+++ apps/web/src/components/Ingredient/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export interface IngredientsProps {
+  brand: string;
+  name: string;
+  macros: {
+    calories: number;
+    carbs: number;
+    prots: number;
+    fats: number;
+  }
+  amount: number;
+  unit: {
+    name: string;
+    short: string;
+  }
+}
+
+export const Ingredient: React.FC<IngredientsProps> = (ingredient: IngredientsProps) => {
+  return (
+    <Container>
+      <div className="header">
+        <h5>{ingredient.brand}</h5>
+        <h4>{ingredient.name}</h4>
+      </div>
+      <div className="amount">
+        <div className="macros">
+          <span>{ingredient.macros.calories}kcal</span>
+          <span>C{ingredient.macros.carbs} P{ingredient.macros.prots} F{ingredient.macros.fats}</span>
+        </div>
+        <input type="number" defaultValue={ingredient.amount} />
+      </div>
+    </Container>
+  )
+}
