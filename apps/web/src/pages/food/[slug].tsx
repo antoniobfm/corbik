7f4aec36d1d0a6eaf981c21320ff30884577088d
antoniobfm <toinho111@gmail.com>
2023-01-11 11:24:33 -0300

    Added When module component to Food page

--- apps/web/src/pages/food/[slug].tsx
+++ apps/web/src/pages/food/[slug].tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Container } from '@/modules/Food/styles';
 import { MacroHeader } from '@/components/MacroHeader';
 import Header from '@/modules/Food/Header';
+import When from '@/modules/Food/When';
 
 const Food: React.FC = () => {
   return (
     <Container>
       <Header name="Banana" brand="Nature" />
       <MacroHeader carb={50} prot={20} fat={10} calorie={100} />
+      <When />
     </Container>
   )
 }
