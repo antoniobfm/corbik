218fa9823f3e9cff37e3fc77ca9f4a3bebd041a6
antoniobfm <toinho111@gmail.com>
2023-01-10 23:26:24 -0300

    Created Food module component: When

---
+++ apps/web/src/modules/Food/When/Food.When.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import When from './index'
+
+export default {
+  title: 'Pages/Food/When',
+  component: When,
+} as ComponentMeta<typeof When>;
+
+
+const Template: ComponentStory<typeof When> = (args) => <When {...args} />;
+
+export const Simple = Template.bind({});
+
+Simple.args = {
+};
---
+++ apps/web/src/modules/Food/When/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const When: React.FC = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default When;
