fd82cb1679f696c72504c59659139374154fd20e
antoniobfm <toinho111@gmail.com>
2023-01-12 00:47:30 -0300

    Added "Ingredients" module component to "Food" page

--- apps/web/src/pages/food/[slug].tsx
+++ apps/web/src/pages/food/[slug].tsx
@@ -4,6 +4,7 @@ import { Container } from '@/modules/Food/styles';
 import { MacroHeader } from '@/components/MacroHeader';
 import Header from '@/modules/Food/Header';
 import When from '@/modules/Food/When';
+import Ingredients from '@/modules/Food/Ingredients';
 
 const Food: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Food: React.FC = () => {
       <Header name="Banana" brand="Nature" />
       <MacroHeader carb={50} prot={20} fat={10} calorie={100} />
       <When />
+      <Ingredients />
     </Container>
   )
 }
