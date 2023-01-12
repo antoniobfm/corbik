d3167b138ab74867aec3464bdb0a270b4917e7f7
antoniobfm <toinho111@gmail.com>
2023-01-12 12:40:36 -0300

    Added "History" module component to "Food" page

--- apps/web/src/pages/food/[slug].tsx
+++ apps/web/src/pages/food/[slug].tsx
@@ -5,6 +5,7 @@ import { MacroHeader } from '@/components/MacroHeader';
 import Header from '@/modules/Food/Header';
 import When from '@/modules/Food/When';
 import Ingredients from '@/modules/Food/Ingredients';
+import History from '@/modules/Food/History';
 
 const Food: React.FC = () => {
   return (
@@ -13,6 +14,22 @@ const Food: React.FC = () => {
       <MacroHeader carb={50} prot={20} fat={10} calorie={100} />
       <When />
       <Ingredients />
+      <History history={[{
+        when: "2021-01-01",
+        amount: 100,
+        unit: {
+          name: "gram",
+          short: "g"
+        }
+        },
+        {
+          when: "2021-01-02",
+          amount: 200,
+          unit: {
+            name: "gram",
+            short: "g"
+          }
+        }]} />
     </Container>
   )
 }
