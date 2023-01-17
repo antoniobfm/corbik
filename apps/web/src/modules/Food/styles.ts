6622fe666e8ffc52f6672172232d862366e7e86e
antoniobfm <toinho111@gmail.com>
2023-01-17 12:58:41 -0300

    Added "LogBar" module component to "Food" page and added padding-bottom to page

--- apps/web/src/modules/Food/styles.ts
+++ apps/web/src/modules/Food/styles.ts
@@ -4,4 +4,6 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
+
+  padding-bottom: 20vh;
 `;
--- apps/web/src/pages/food/[slug].tsx
+++ apps/web/src/pages/food/[slug].tsx
@@ -6,10 +6,12 @@ import Header from '@/modules/Food/Header';
 import When from '@/modules/Food/When';
 import Ingredients from '@/modules/Food/Ingredients';
 import History from '@/modules/Food/History';
+import LogBar from '@/modules/Food/LogBar';
 
 const Food: React.FC = () => {
   return (
     <Container>
+      <LogBar />
       <Header name="Banana" brand="Nature" />
       <MacroHeader carb={50} prot={20} fat={10} calorie={100} />
       <When />
