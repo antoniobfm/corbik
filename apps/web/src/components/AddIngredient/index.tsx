c392ec506a5db971aa600da1548bc0f13b61fd20
antoniobfm <toinho111@gmail.com>
2023-01-24 01:08:59 -0300

    Added Library to AddIngredient

--- apps/web/src/components/AddIngredient/index.tsx
+++ apps/web/src/components/AddIngredient/index.tsx
@@ -1,14 +1,21 @@
+import MyLibrary from '@/modules/Library/MyLibrary';
 import React from 'react';
-import { CurrentSelectedIngredient } from './CurrentSelectedIngredient';
+import { FoodListing } from '../FoodListing';
 
 
-import { Container } from './styles';
+import { Container, Library } from './styles';
 
 export const AddIngredient: React.FC = () => {
 
   return (
     <Container>
-      <CurrentSelectedIngredient />
+      <Library>
+        <FoodListing brand='Italiano' name='Parmegiano' macros={{ carbs: 5, prots: 2, fats: 3 }} calories={100} key='0' />
+        <FoodListing brand='Mother Nature' name='Apple' macros={{ carbs: 5, prots: 2, fats: 3 }} calories={400} key='1' />
+        <FoodListing brand='Mother Nature' name='Banana' macros={{ carbs: 5, prots: 2, fats: 3 }} calories={850} key='2' />
+        <FoodListing brand='Mother Nature' name='Alecrim' macros={{ carbs: 5, prots: 2, fats: 3 }} calories={50} key='2' />
+        <FoodListing brand='Mother Nature' name='Tomato' macros={{ carbs: 5, prots: 2, fats: 3 }} calories={440} key='2' />
+      </Library>
     </Container>
   )
 }
--- apps/web/src/components/AddIngredient/styles.ts
+++ apps/web/src/components/AddIngredient/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const Container = styled.button``;
+export const Container = styled.div``;
+
+export const Library = styled.div`
+  background: #181A1B;
+  border: 1px solid #222425;
+  border-radius: 6px;
+
+  display: flex;
+  flex-direction: column;
+  
+`;
