e2a9072e41d050a250aab23df881a050870b43c2
antoniobfm <toinho111@gmail.com>
2023-01-28 18:45:10 -0300

    Fixed errors in "PublicLibrary" Dashboard's module component

--- apps/web/src/modules/Library/PublicLibrary/index.tsx
+++ apps/web/src/modules/Library/PublicLibrary/index.tsx
@@ -3,18 +3,21 @@ import React from 'react';
 import { FoodListing } from '@/components/FoodListing';
 
 import { Container, List } from './styles';
+import { useRouter } from 'next/router';
 
 const PublicLibrary: React.FC = () => {
+  const router = useRouter();
+
   return (
     <Container>
       <div className="header">
         <h2>Public Library</h2>
       </div>
       <List>
-        <FoodListing name='Maçã' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} onClick={() => router.push('/food/test')} />
-        <FoodListing name='Banana' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} onClick={() => router.push('/food/test')} />
-        <FoodListing name='Abacaxi' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} onClick={() => router.push('/food/test')} />
-        <FoodListing name='Maçã' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} onClick={() => router.push('/food/test')} />
+        <FoodListing name='Maçã' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} calories={200} onClick={() => router.push('/food/test')} />
+        <FoodListing name='Banana' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} calories={200} onClick={() => router.push('/food/test')} />
+        <FoodListing name='Abacaxi' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} calories={200} onClick={() => router.push('/food/test')} />
+        <FoodListing name='Maçã' brand='Nature' macros={{carbs: 50, prots: 20, fats: 10}} calories={200} onClick={() => router.push('/food/test')} />
       </List>
     </Container>
   )
