5826403b86d829511be28824e9b6212331ef1d0c
antoniobfm <toinho111@gmail.com>
2023-01-28 20:09:23 -0300

    Created and Added "When" module component to Logs module and page

---
+++ apps/web/src/modules/Logs/When/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  padding: 24px;
+
+  background: #181A1B;
+  border: 1px solid #222425;
+`;
--- apps/web/src/pages/diet/logs/[slug].tsx
+++ apps/web/src/pages/diet/logs/[slug].tsx
@@ -5,6 +5,7 @@ import LogBar from '@/components/LogBar';
 import { units } from '@/data/units';
 import Header from '@/modules/Logs/Header';
 import { MacroHeader } from '@/components/MacroHeader';
+import When from '@/modules/Logs/When';
 
 const logs: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const logs: React.FC = () => {
       <Header name='Banana' brand="Nature" />
       <LogBar type="update" units={units} />
       <MacroHeader carb={50} prot={20} fat={10} calorie={100} />
+      <When />
     </Container>
   )
 }
