f26d36df3f6de562c2b3633ab5dfff748a256b39
antoniobfm <toinho111@gmail.com>
2023-01-28 20:01:40 -0300

    Added "MacroHeader" shared component to Logs page

--- apps/web/src/pages/diet/logs/[slug].tsx
+++ apps/web/src/pages/diet/logs/[slug].tsx
@@ -4,12 +4,14 @@ import { Container } from '@/modules/Logs/styles';
 import LogBar from '@/components/LogBar';
 import { units } from '@/data/units';
 import Header from '@/modules/Logs/Header';
+import { MacroHeader } from '@/components/MacroHeader';
 
 const logs: React.FC = () => {
   return (
     <Container>
       <Header name='Banana' brand="Nature" />
       <LogBar type="update" units={units} />
+      <MacroHeader carb={50} prot={20} fat={10} calorie={100} />
     </Container>
   )
 }
