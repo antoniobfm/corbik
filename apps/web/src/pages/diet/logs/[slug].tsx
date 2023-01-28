181e62d687aeed93e7217e067b1d889a85f1d29d
antoniobfm <toinho111@gmail.com>
2023-01-28 19:22:51 -0300

    Added "LogBar" shared component to Logs page

--- apps/web/src/pages/diet/logs/[slug].tsx
+++ apps/web/src/pages/diet/logs/[slug].tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { Container } from '@/modules/Logs/styles';
+import LogBar from '@/components/LogBar';
+import { units } from '@/data/units';
 
 const logs: React.FC = () => {
   return (
     <Container>
-
+      <LogBar type="update" units={units} />
     </Container>
   )
 }
