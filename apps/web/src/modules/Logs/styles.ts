a76408f47e7748573a4b0920c044fc61e0134592
antoniobfm <toinho111@gmail.com>
2023-01-28 20:30:53 -0300

    Spaced components in Logs page and compensated MacroHeader by -24px margin-bottom

--- apps/web/src/modules/Logs/styles.ts
+++ apps/web/src/modules/Logs/styles.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  padding-bottom: 20vh;
+
+  // Affects MacroHeader component, or it is supposed to
+  > div:nth-child(3) {
+    margin-bottom: -24px;
+  }
 `;
