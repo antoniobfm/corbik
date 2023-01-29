632d3693ce91c07b7e418d31f88e61f3fcceb867
antoniobfm <toinho111@gmail.com>
2023-01-29 20:02:26 -0300

    Implemented CreateFood > Form > Units > UnitListing and Added it to parent

--- apps/web/src/modules/CreateFood/Form/Units/UnitListing/index.tsx
+++ apps/web/src/modules/CreateFood/Form/Units/UnitListing/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const UnitListing: React.FC = () => {
+export interface UnitListingProps {
+  name: string;
+  amount: number;
+}
+
+export const UnitListing: React.FC<UnitListingProps> = (unit: UnitListingProps) => {
   return (
     <Container>
-      
+      <h4>{unit.name}</h4>
+      <input type="number" defaultValue={unit.amount} placeholder="Amount" />
     </Container>
   )
 }
-
-export default UnitListing;
--- apps/web/src/modules/CreateFood/Form/Units/index.tsx
+++ apps/web/src/modules/CreateFood/Form/Units/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { Container } from './styles';
-import UnitListing from './UnitListing';
+import { UnitListing } from './UnitListing';
 
 const Units: React.FC = () => {
   return (
     <Container>
       <h3>Units</h3>
-      <UnitListing />
+      <div className="list">
+        <UnitListing name="Grams" amount={100} />
+        <UnitListing name="Grams" amount={100} />
+      </div>
     </Container>
   )
 }
