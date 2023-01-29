37daa8936f750036a732550be1e3bd5f8966399d
antoniobfm <toinho111@gmail.com>
2023-01-28 21:46:25 -0300

    Created and Added Header module component to CreateFood's module and page

---
+++ apps/web/src/modules/CreateFood/Header/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  padding: 24px 24px 0px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 10px;
+    line-height: 15px;
+    /* identical to box height */
+
+    display: flex;
+    align-items: center;
+    text-align: right;
+
+    color: #F2F2F2;
+
+    opacity: 0.5;
+  }
+`;
--- apps/web/src/pages/food/create.tsx
+++ apps/web/src/pages/food/create.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import { Container } from '@/modules/CreateFood/styles';
+import Header from '@/modules/CreateFood/Header';
 
 const food: React.FC = () => {
   return (
     <Container>
-
+      <Header />
     </Container>
   )
 }
