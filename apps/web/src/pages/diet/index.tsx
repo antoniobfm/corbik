5fdc8d975f99004ae711f33a725d0b8ba0e4c70f
antoniobfm <toinho111@gmail.com>
2023-01-29 19:09:45 -0300

    Added shared component "Navbar" to Diet dashboard page

--- apps/web/src/pages/diet/index.tsx
+++ apps/web/src/pages/diet/index.tsx
@@ -4,6 +4,7 @@ import { MacroHeader } from '@/components/MacroHeader';
 import Header from '@/modules/Dashboard/Header';
 import LogList from '@/modules/Dashboard/LogList';
 import { Container } from '@/modules/Dashboard/styles';
+import { Navbar } from '@/components/Navbar';
 
 const Diet: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Diet: React.FC = () => {
       <Header />
       <MacroHeader carb={55} fat={20} prot={10} calorie={100} />
       <LogList />
+      <Navbar active="diet" />
     </Container>
   )
 }
