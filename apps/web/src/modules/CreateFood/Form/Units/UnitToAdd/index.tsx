e3ff2badd40e3d3a6430922180f9eb24dee8220c
antoniobfm <toinho111@gmail.com>
2023-01-29 20:22:51 -0300

    Added AddCustom to CreateFood > Form > Units > UnitToAdd

--- apps/web/src/modules/CreateFood/Form/Units/UnitToAdd/index.tsx
+++ apps/web/src/modules/CreateFood/Form/Units/UnitToAdd/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, UnitOption } from './styles';
+import { AddCustom, Container, UnitOption } from './styles';
 
 const UnitToAdd: React.FC = () => {
   return (
@@ -23,7 +23,24 @@ const UnitToAdd: React.FC = () => {
           <h4>Milliliter (ml)</h4>
           <span>Metric</span>
         </UnitOption>
+        <UnitOption>
+          <h4>Pound mass (lb)</h4>
+          <span>Imperial</span>
+        </UnitOption>
+        <UnitOption>
+          <h4>Ounce mass (oz)</h4>
+          <span>Imperial</span>
+        </UnitOption>
       </div>
+      <AddCustom>
+        <input type="text" placeholder="Add custom unit" />
+        <button type="button">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 3.33325V12.6666" stroke="#D5F6E3" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M3.3335 8H12.6668" stroke="#D5F6E3" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </button>
+      </AddCustom>
     </Container>
   )
 }
--- apps/web/src/modules/CreateFood/Form/Units/UnitToAdd/styles.ts
+++ apps/web/src/modules/CreateFood/Form/Units/UnitToAdd/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.div`display: flex;
   border-radius: 6px;
 
   h3 {
-    padding: 24px 24px 0px;
+    padding: 24px 24px 16px;
   }
 `;
 
@@ -49,5 +49,46 @@ export const UnitOption = styled.div`
     /* 4. Medium Emphasis */
 
     color: #999999;
+  }
+`;
+
+export const AddCustom = styled.div` 
+  display: flex;
+
+  input {
+    padding-left: 24px;
+
+    flex: 1;
+
+    outline: none;
+    border: none;
+
+    background: linear-gradient(92.01deg, rgba(255, 255, 255, 0.03) 0%, rgba(24, 26, 27, 0) 62.45%), rgba(11, 11, 12, 0.6);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
+    border-radius: 0px 0px 6px 6px;
+
+    font-family: 'Futura PT';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    display: flex;
+    align-items: center;
+
+    color: #FFFFFF;
+
+    &::placeholder {
+      opacity: 0.4;
+    }
+  }
+
+  button {
+    width: 80px;
+    height: 48px;
+
+    border-radius: 0px 0px 6px 0px;
+    border: none;
+    outline: none;
+    background: linear-gradient(113.21deg, rgba(39, 174, 96, 0.5) 0%, rgba(39, 174, 96, 0.25) 96.25%);
   }
 `;
