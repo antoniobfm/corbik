ba4c554f2736d503c4bb383468a106dbad5b0233
antoniobfm <toinho111@gmail.com>
2023-02-01 18:03:55 -0300

    Now can turn Modal on or off in CreateFood > Form > Units

--- apps/web/src/modules/CreateFood/Form/Units/index.tsx
+++ apps/web/src/modules/CreateFood/Form/Units/index.tsx
@@ -1,23 +1,27 @@
 import Modal from '@/components/Modal';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
 import { UnitListing } from './UnitListing';
 import UnitToAdd from './UnitToAdd';
 
 const Units: React.FC = () => {
+  const [active, setActive] = useState(true)
+
   return (
     <>
-      <Modal>
-        <UnitToAdd />
-      </Modal>
+      {active &&
+        <Modal setActive={setActive}>
+          <UnitToAdd />
+        </Modal>
+      }
       <Container>
         <h3>Units</h3>
         <div className="list">
           <UnitListing name="Grams" amount={100} />
           <UnitListing name="Grams" amount={100} />
         </div>
-        <button type="button">ADD UNIT</button>
+        <button type="button" onClick={() => setActive(true)}>ADD UNIT</button>
       </Container>
     </>
   )
