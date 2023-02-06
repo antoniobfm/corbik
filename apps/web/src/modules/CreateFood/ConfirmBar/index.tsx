8799648599645c49bd89c5c8a00c085a0e68c299
antoniobfm <toinho111@gmail.com>
2023-02-06 16:46:35 -0300

    CreateFood > ConfirmBar - Now GoBack button makes sends user back to previous page onClick

--- apps/web/src/modules/CreateFood/ConfirmBar/index.tsx
+++ apps/web/src/modules/CreateFood/ConfirmBar/index.tsx
@@ -1,14 +1,17 @@
 import { ConfirmActionButton } from '@/components/LogBar/styles';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { Container, GoBack, Menu } from './styles';
 
 const ConfirmBar: React.FC = () => {
+  const router = useRouter();
+
   return (
     <Container>
       <div>
         <Menu>
-          <GoBack>
+          <GoBack onClick={() => router.back()}>
           <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path opacity="0.5" d="M6 11L1 6L6 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
